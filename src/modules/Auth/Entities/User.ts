24db38c8ba9129e785f49eee3382cf58e5b75765
ericNKS <ek.silva.santos@gmail.com>
2025-05-06 09:12:26 -0300

    feat: configurate database

---
+++ src/modules/Auth/Entities/User.ts
@@ -0,0 +1,10 @@
+export default class User {
+    constructor(
+        public readonly id: string,
+        public name: string,
+        public email: string,
+        public password: string,
+        public rules: Array<string>,
+    ) {}
+    
+}
