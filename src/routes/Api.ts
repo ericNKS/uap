749e6ebe6350ed10c89f4ca7acf6a67974c85812
ericNKS <ek.silva.santos@gmail.com>
2025-05-06 06:15:40 -0300

    feat: add not found route

--- src/routes/Api.ts
+++ src/routes/Api.ts
@@ -1,8 +1,14 @@
 import { Express, Request, Response } from "express";
 export default function Api(app: Express) {
-    app.get('/', (req: Request, res: Response) => {
+    app.get('/api', (req: Request, res: Response) => {
         res.json({
             ping: "pong"
         });
+    })
+
+    app.use((req: Request, res: Response) => {
+        res.status(404).json({
+            message: 'Ohh you are lost, read the API documentation to find your way back home :)'
+        })
     })
 }
