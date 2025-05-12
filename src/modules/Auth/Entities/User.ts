b2eada354462e08b912317c707a3e4b3dce56680
ericNKS <ek.silva.santos@gmail.com>
2025-05-12 16:35:42 -0300

    filx: corrigindo entity para seguir a modelagem

--- src/modules/Auth/Entities/User.ts
+++ src/modules/Auth/Entities/User.ts
@@ -1,10 +1,27 @@
 export default class User {
-    public readonly id?: number;
-    constructor(
-        public name?: string,
-        public email?: string,
-        public password?: string,
-        public rules?: Array<string>,
-    ) {}
-    
+    public readonly idUser: number = 0;
+    public nomeuser: string = '';
+    public emailuser: string = '';
+    public senhauser: string = '';
+    public teluser: string = '';
+    public cpforcnpjuser: string = '';
+    public imgurluser: string = '';
+    public genuser: string = '';
+    public rulesuser: string = '';
+    public stsativouser: string = '';
+
+    public toJson(): User {
+        return {
+            idUser: this.idUser,
+            nomeuser: this.nomeuser,
+            emailuser: this.emailuser,
+            senhauser: this.senhauser,
+            teluser: this.teluser,
+            cpforcnpjuser: this.cpforcnpjuser,
+            imgurluser: this.imgurluser,
+            genuser: this.genuser,
+            rulesuser: this.rulesuser,
+            stsativouser: this.stsativouser,
+        } as User
+    }
 }
