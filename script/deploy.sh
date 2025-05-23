55e5a7980f95adc6d3534d3ba72ec6beae828780
ericNKS <ek.silva.santos@gmail.com>
2025-05-22 21:09:21 -0300

    fix: corrigindo git

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git pull origin main
-docker-compose -f ../docker-compose.yml up redis rabbitmq api-send-mail service-send-mail
+git -C /home/google/uap pull origin main
+docker-compose -f /home/google/uap/docker-compose.yml up redis rabbitmq api-send-mail service-send-mail
 docker build -t app ../
 docker stop app
 docker run --name app -d -p 80:3000 app
