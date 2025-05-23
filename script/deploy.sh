b03eb608eb722616fe65ba935df5dc3053817b38
ericNKS <ek.silva.santos@gmail.com>
2025-05-22 21:11:11 -0300

    fix: corrigindo o deploy infinito

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git -C /home/google/uap pull origin main
-docker-compose -f /home/google/uap/docker-compose.yml up redis rabbitmq api-send-mail service-send-mail
+docker-compose -f /home/google/uap/docker-compose.yml up redis rabbitmq api-send-mail service-send-mail -d
 docker build -t app ../
 docker stop app
 docker run --name app -d -p 80:3000 app
