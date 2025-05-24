76bf8dcf1b97c6ce66793a30b2b63e4e6a03370a
Guilherme Kalleu Novais <guilhermekalleuofc@gmail.com>
2025-05-23 21:00:42 -0300

    Atualização do Banco de Dados

--- db/store query.sql
+++ db/store query.sql
@@ -18,6 +18,7 @@ ImgUrlUser TEXT,
 GenUser VARCHAR(30) NOT NULL,
 PronomeUser VARCHAR(10) NOT NULL,
 RulesUser VARCHAR(30) NOT NULL DEFAULT 'RULE_PACIENTE',
+StsVerificarEmail BOOLEAN NOT NULL DEFAULT FALSE,
 StsAtivoUser CHAR(1) NOT NULL
 );
 
@@ -29,6 +30,7 @@ IdEspecialista BIGINT,
 DtConsulta DATE NOT NULL,
 HrConsulta TIME NOT NULL,
 InfoConsulta TEXT,
+StsAtivoConsulta CHAR(1) NOT NULL DEFAULT 'n',
 FOREIGN KEY(IdPaciente) REFERENCES users(IdUser),
 FOREIGN KEY(IdEspecialista) REFERENCES users(IdUser)
 );
@@ -38,8 +40,7 @@ CREATE TABLE if NOT EXISTS expediente(
 IdExpediente BIGINT AUTO_INCREMENT PRIMARY KEY,
 IdUser BIGINT,
 DtExpediente DATE NOT NULL,
-HrInicioExpediente TIME NOT NULL,
-HrFimExpediente TIME NOT NULL,
+HrExpediente TIME NOT NULL,
 StsAtivoExpediente CHAR(1) NOT NULL DEFAULT 's',
 FOREIGN KEY(IdUser) REFERENCES users(IdUser)
 );
@@ -81,7 +82,7 @@ CREATE PROCEDURE spRegistrarPaciente(
 BEGIN
 	DECLARE vIdUser BIGINT DEFAULT 0;
 	
-	SELECT if(COUNT(u.IdUser) = 0, 0, 1) INTO vIdUser
+	SELECT if(COUNT(u.IdUser) = 0,0, 1) INTO vIdUser
 	FROM users u
 	WHERE pCpfOrCnpjUser = u.CpfOrCnpjUser
 	GROUP BY(u.IdUser);
@@ -108,7 +109,7 @@ CREATE PROCEDURE spRegistrarEspecialista(
 BEGIN
 	DECLARE vIdUser BIGINT DEFAULT 0;
 	
-	SELECT if(COUNT(u.IdUser) = 0, 0, 1) INTO vIdUser
+	SELECT if(COUNT(u.IdUser) = 0,0, 1) INTO vIdUser
 	FROM users u
 	WHERE pCpfOrCnpjUser = u.CpfOrCnpjUser
 	GROUP BY(u.IdUser);
@@ -118,6 +119,50 @@ BEGIN
 		VALUES (pNomeUser, pEmailUser, pSenhaUser, pTelUser, pCpfOrCnpjUser, pCrpUser, pGenUser, pPronomeUser, 'RULE_ESPECIALISTA_PENDENTE', 's');
 	END if;
 END
+$$
+
+
+delimiter $$
+CREATE PROCEDURE spAtivarEmail(
+	IN pIdUser BIGINT
+)
+BEGIN
+	DECLARE vIdUser BIGINT DEFAULT 0;
+
+SELECT if(COUNT(u.IdUser), <> 1,0, u.IdUser) INTO vIdUser
+FROM users u
+WHERE pIduser = u.IdUser
+GROUP BY(u.IdUser);	
+	
+	IF vIdUser  = pIdUser then
+		UPDATE users
+		SET StsVerificarEmail = true
+		WHERE u.IdUser = vIdUser;
+	END if;
+END
+$$
+
+
+delimiter $$
+CREATE PROCEDURE spAdicionarImgUsuario(
+	IN pIdUser BIGINT,
+	IN pImgUrlUser TEXT
+)
+BEGIN
+	DECLARE vIdUser BIGINT DEFAULT 0;
+	
+	SELECT if(COUNT(u.IdUser) <> 1, 0, u.IdUser) INTO vIdUser
+	FROM users u
+	WHERE pIdUser = u.IdUser
+	AND u.StsAtivoUser = 's'
+	GROUP BY(u.IdUser);
+	
+	if vIdUser = pIdUser then
+		UPDATE users
+		SET ImgUrlUser = pImgUrlUser
+		WHERE IdUser = vIdUser;
+	END if;
+END 
 $$
 
 
@@ -136,12 +181,12 @@ $$
 delimiter $$
 CREATE PROCEDURE spListarPacientes()
 BEGIN
-	SELECT u.IdUser AS 'ID do Paciente',
-			 u.NomeUser AS 'Nome do Paciente',
-			 u.EmailUser AS 'Email do Paciente', 
-			 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CPF/CNPJ do Paciente',
-			 u.GenUser AS 'Gênero do Paciente',
-			 u.pPronomeUser AS 'Pronome do Paciente'
+	SELECT u.IdUser,
+			 u.NomeUser,
+			 u.EmailUser, 
+			 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CpfOrCnpjUser',
+			 u.GenUser,
+			 u.pPronomeUser
 	FROM users u
 	WHERE u.StsAtivoUser = 's'
 	AND u.RulesUser = 'RULE_PACIENTE';	
@@ -152,14 +197,14 @@ $$
 delimiter $$
 CREATE PROCEDURE spListarEspecialistasAtivos()
 BEGIN
-	SELECT u.IdUser AS 'ID do Especialista',
-			 u.NomeUser AS 'Nome do Especialista',
-			 u.EmailUser AS 'Email do Especialista', 
-			 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CPF/CNPJ do Especialista',
-			 sfFormatarCrp(u.CrpUser) AS 'CRP do Especialista',
-			 u.ImgUrlUser AS 'Imagem do Especialista',
-			 u.GenUser AS 'Gênero do Especialista',
-			 u.pPronomeUser AS 'Pronome do Especialista'
+	SELECT u.IdUser,
+			 u.NomeUser,
+			 u.EmailUser, 
+			 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CpfOrCnpjUser',
+			 sfFormatarCrp(u.CrpUser) AS 'CrpUser',
+			 u.ImgUrlUser,
+			 u.GenUser,
+			 u.pPronomeUser
 	FROM users u
 	WHERE u.StsAtivoUser = 's'
 	AND u.RulesUser = 'RULE_ESPECIALISTA_ATIVO';	
@@ -170,14 +215,14 @@ $$
 delimiter $$
 CREATE PROCEDURE spListarEspecialistasPendentes()
 BEGIN
-	SELECT u.IdUser AS 'ID do Especialista',
-			 u.NomeUser AS 'Nome do Especialista',
-			 u.EmailUser AS 'Email do Especialista', 
-			 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CPF/CNPJ do Especialista',
-			 sfFormatarCrp(u.CrpUser) AS 'CRP do Especialista',
-			 u.ImgUrlUser AS 'Imagem do Especialista',
-			 u.GenUser AS 'Gênero do Especialista',
-			 u.pPronomeUser AS 'Pronome do Especialista'
+	SELECT u.IdUser,
+			 u.NomeUser,
+			 u.EmailUser, 
+			 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CpfOrCnpjUser',
+			 sfFormatarCrp(u.CrpUser) AS 'CrpUser',
+			 u.ImgUrlUser,
+			 u.GenUser,
+			 u.pPronomeUser
 	FROM users u
 	WHERE u.StsAtivoUser = 's'
 	AND u.RulesUser = 'RULE_ESPECIALISTA_PENDENTE';	
@@ -188,15 +233,98 @@ $$
 delimiter $$
 CREATE PROCEDURE spListarUsuariosExcluidos()
 BEGIN
-	SELECT u.IdUser AS 'ID do Usuário',
-			 u.NomeUser AS 'Nome do Usuário',
-			 u.EmailUser AS 'Email do Usuário',
-			 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CPF/CNPJ do Usuário',
-			 u.GenUser AS 'Gênero do Usuário',
-			 u.pPronomeUser AS 'Pronome do UsuáriospListarPacientesspListarPacientes'
+	SELECT u.IdUser,
+			 u.NomeUser,
+			 u.EmailUser,
+			 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CpfOrCnpjUser',
+			 u.GenUser,
+			 u.pPronomeUser
 	FROM users u
 	WHERE u.StsAtivoUser = 'n';	
 END
+$$
+
+
+delimiter $$
+CREATE PROCEDURE spPegarUserEmail(
+	IN pEmailUser VARCHAR(100)
+)
+BEGIN
+	DECLARE vEmailUser VARCHAR(100) DEFAULT '';
+	
+	SELECT if(COUNT(u.EmailUser) <> 1, 0 , u.EmailUser) INTO vEmailUser
+	FROM users u
+	WHERE pEmailUser = u.EmailUser
+	GROUP BY(u.EmailUser);
+	
+	if vEmailUser = pEmailUser then
+		SELECT u.IdUser,
+				 u.NomeUser,
+				 u.EmailUser,
+				 u.SenhaUser,
+				 u.ImgUrlUser,
+				 u.GenUser,
+				 u.PronomeUser,
+				 u.RulesUser,
+				 u.StsVerificarEmail,
+				 u.StsAtivoUser
+		FROM users u
+		WHERE  vEmailUser = u.EmailUser;
+	END if;
+END 
+$$
+
+
+delimiter $$
+CREATE PROCEDURE spPegarUserCpfOrCnpj(
+	IN pCpfOrCnpjUser VARCHAR(20)
+)
+BEGIN
+	DECLARE vCpfOrCnpjUser VARCHAR(20) DEFAULT '';
+	
+	SELECT if(COUNT(u.CpfOrCnpjUser) <> 1, 0 , u.CpfOrCnpjUser) INTO vCpfOrCnpjUser
+	FROM users u
+	WHERE pCpfOrCnpjUser = u.CpfOrCnpjUser
+	GROUP BY(u.CpfOrCnpjUser);
+	
+	if vCpfOrCnpjUser = pCpfOrCnpjUser then
+		SELECT u.IdUser
+		FROM users u
+		WHERE vCpfOrCnpjUser = u.CpfOrCnpjUser;
+	END if;
+END 
+$$
+
+
+delimiter $$
+CREATE PROCEDURE spPegarUserId(
+	IN pIdUser BIGINT
+)
+BEGIN
+	DECLARE vIdUser BIGINT DEFAULT 0;
+	
+	SELECT if(COUNT(u.IdUser) <> 1, 0 , u.IdUser) INTO vIdUser
+	FROM users u
+	WHERE pIdUser = u.IdUser
+	GROUP BY(u.IdUser);
+	
+	if vIdUser = pIdUser then
+		SELECT u.IdUser,
+				 u.NomeUser,
+				 u.EmailUser,
+				 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS CpfOrCnpjUser,
+				 sfFormatarTel(u.TelUser) AS TelUser,
+				 sfFormatarCrp(u.CrpUser) AS CrpUser,
+				 u.ImgUrlUser,
+				 u.GenUser,
+				 u.PronomeUser,
+				 u.RulesUser,
+				 u.StsVerificarEmail,
+				 u.StsAtivoUser
+		FROM users u
+		WHERE  vIdUser = u.IdUser;
+	END if;
+END 
 $$
 
 
@@ -228,8 +356,7 @@ delimiter $$
 CREATE PROCEDURE spAdicionarExpediente (
 	IN pIdUser BIGINT,
 	IN pDtExpediente DATE,
-	IN pHrInicioExpediente TIME,
-	IN pHrFimExpediente TIME
+	IN pHrExpediente TIME,
 )
 BEGIN
 	DECLARE vIdUser BIGINT DEFAULT 0;
@@ -240,8 +367,8 @@ BEGIN
 	GROUP BY(u.IdUser);
 	
 	if vIdUser = pIdUser then
-		INSERT INTO expediente (IdUser, DtExpediente, HrInicioExpediente, HrFimExpediente)
-		VALUES(pIdUser, pDtExpediente, pHrInicioExpediente, pHrFimExpediente);
+		INSERT INTO expediente (IdUser, DtExpediente, HrExpediente)
+		VALUES(pIdUser, pDtExpediente, pHrExpediente);
 	END if;
 END
 $$
@@ -276,6 +403,128 @@ BEGIN
 		SET StsAtivoExpediente = 'n'
 		WHERE vIdUser = IdUser
 		AND pIdExpediente = IdExpediente;
+	END if;
+END
+$$
+
+delimiter $$
+CREATE PROCEDURE spListarExpedientes(
+	IN pIdUser BIGINT
+)
+BEGIN
+	DECLARE vIdUser BIGINT DEFAULT 0;
+
+	SELECT if(COUNT(u.IdUser) <> 1, 0 , u.IdUser) INTO vIdUser
+	FROM users u
+	WHERE pIduser = u.IdUser
+	AND u.RulesUser = 'RULE_ESPECIALISTA_ATIVO'
+	GROUP BY(u.IdUser);
+	
+	if vIdUser = pIdUser then
+		SELECT e.IdExpediente,
+			 	 e.DtExpediente,
+			 	 e.HrExpediente
+		FROM expediente e JOIN users u
+								ON e.IdUser = u.IdUser
+		WHERE e.IdUser = vIdUser
+		ORDER BY e.DtExpediente ASC,
+					e.HrExpediente ASC;
+	END if;
+END
+$$
+
+
+delimiter $$
+CREATE PROCEDURE spRegistrarConsulta(
+	IN pIdPaciente BIGINT,
+	IN pIdEspecialista BIGINT,
+	IN pDtConsulta DATE,
+	IN pHrConsulta TIME,
+	IN pInfoConsluta TEXT
+)
+BEGIN
+	DECLARE vIdPaciente BIGINT DEFAULT 0;
+	DECLARE vIdEspecialista BIGINT DEFAULT 0;
+	
+	SELECT if(COUNT(u.IdUser) <> 1, 0 , u.IdUser) INTO vIdPaciente
+	FROM users u
+	WHERE pIdPaciente = u.IdUser
+	AND u.StsAtivoUser = 's'
+	AND u.RulesUser = 'RULE_PACIENTE'
+	GROUP BY(u.IdUser);
+	
+	if vIdPaciente = pIdPaciente then
+		SELECT if(COUNT(u.IdUser) <> 1, 0 , u.IdUser) INTO vIdEspecialista
+		FROM users u
+		WHERE pIdEspecialista = u.IdUser
+		AND u.StsAtivoUser = 's'
+		AND u.RulesUser = 'RULE_ESPECIALISTA_ATIVO'
+		GROUP BY(u.IdUser);
+	END if;
+	
+	if vIdEspecialista = pIdEspecialista then
+		SELECT if(COUNT(e.IdUser) <> 1, 0 , e.IdUser) INTO vIdEspecialista
+		FROM expediente e
+		WHERE pIdEspecialista = e.IdUser
+		AND pDtConsulta = e.DtExpediente
+		AND pHrConsulta = e.HrExpediente
+		AND e.StsAtivoExpediente = 's'
+		GROUP BY(e.IdUser);
+	END if;
+	
+	if vIdEspecialista = pIdEspecialista then
+		INSERT INTO consultas(IdPaciente, IdEspecialista, DtConsulta, HrConsulta, InfoConsulta)
+		VALUES (pIdPaciente, pIdEspecialista, pDtConsulta, pHrConsulta, pInfoConsluta);
+	END if;
+END 
+$$
+
+
+delimiter $$
+CREATE PROCEDURE spListarConsultasNaoConfirmadas(
+	IN pIdEspecialista BIGINT
+)
+BEGIN
+	DECLARE vIdEspecialista BIGINT DEFAULT 0;
+	
+	SELECT if(COUNT(u.IdUser) <> 1, 0 , u.IdUser) INTO vIdEspecialista
+	FROM users u
+	WHERE pIdEspecialista = u.IdUser
+	AND u.StsAtivoUser = 's'
+	AND u.RulesUser = 'RULE_ESPECIALISTA_ATIVO'
+	GROUP BY(u.IdUser);
+	
+	if vIdEspecialista = pIdEspecialista then
+		SELECT c.IdConsulta,
+				 u.NomeUser,
+				 u.EmailUser,
+				 c.DtConsulta,
+				 c.HrConsulta,
+				 c.InfoConsulta
+		FROM consultas c JOIN users u
+							ON c.IdPaciente = u.IdUser
+		WHERE c.StsAtivoConsulta = 'n';
+	END if;
+END
+$$
+
+
+delimiter $$
+CREATE PROCEDURE spAtivarConsulta(
+	IN pIdConsulta BIGINT
+)
+BEGIN
+	DECLARE vIdConsulta BIGINT DEFAULT 0;
+	
+	SELECT if(COUNT(c.IdConsulta) <> 1, 0 , c.IdConsulta) INTO vIdConsulta
+	FROM consultas c
+	WHERE pIdConsulta = c.IdConsulta
+	GROUP BY(c.IdConsulta);
+	
+	if vIdConsulta = pIdConsulta then
+		UPDATE consultas
+		SET StsAtivoConsulta = 's'
+		WHERE IdConsulta = vIdConsulta;
 	END if;
 END
 $$
@@ -364,6 +613,23 @@ $$
 
 
 
+-- Criação das Triggers
+
+delimiter $$
+CREATE TRIGGER trDesativarExpedienteAposConsulta
+AFTER INSERT ON consultas
+FOR EACH ROW
+BEGIN
+	UPDATE expediente
+	SET StsAtivoExpediente = 'n'
+   WHERE IdUser = NEW.IdEspecialista
+   AND DtExpediente = NEW.DtConsulta
+   AND HrExpediente = NEW.HrConsulta;
+END
+$$
+
+
+
 -- Criação das Views
 
 CREATE VIEW vEspecialistas_Ativos AS
@@ -373,7 +639,8 @@ SELECT u.IdUser AS 'ID do Especialista',
 		 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CPF/CNPJ do Especialista',
 		 sfFormatarCrp(u.CrpUser) AS 'CRP do Especialista',
 		 u.ImgUrlUser AS 'Imagem do Especialista',
-		 u.GenUser AS 'Gênero do Especialista'
+		 u.GenUser AS 'Gênero do Especialista',
+		 u.PronomeUser AS 'Pronome do Especialista'
 FROM users u
 WHERE u.StsAtivoUser = 's'
 AND u.RulesUser = 'RULE_ESPECIALISTA_ATIVO';
@@ -386,7 +653,8 @@ SELECT u.IdUser AS 'ID do Especialista',
 		 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CPF/CNPJ do Especialista',
 		 sfFormatarCrp(u.CrpUser) AS 'CRP do Especialista',
 		 u.ImgUrlUser AS 'Imagem do Especialista',
-		 u.GenUser AS 'Gênero do Especialista'
+		 u.GenUser AS 'Gênero do Especialista',
+		 u.PronomeUser AS 'Pronome do Especialista'
 FROM users u
 WHERE u.StsAtivoUser = 's'
 AND u.RulesUser = 'RULE_ESPECIALISTA_PENDENTE';
@@ -397,7 +665,8 @@ SELECT u.IdUser AS 'ID do Paciente',
 		 u.NomeUser AS 'Nome do Paciente',
 		 u.EmailUser AS 'Email do Paciente', 
 		 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CPF/CNPJ do Paciente',
-		 u.GenUser AS 'Gênero do Paciente'
+		 u.GenUser AS 'Gênero do Paciente',
+		 u.PronomeUser AS 'Pronome do Paciente'
 FROM users u
 WHERE u.StsAtivoUser = 's'
 AND u.RulesUser = 'RULE_PACIENTE';
@@ -408,6 +677,7 @@ SELECT u.IdUser AS 'ID do Usuário',
 		 u.NomeUser AS 'Nome do Usuário',
 		 u.EmailUser AS 'Email do Usuário',
 		 sfFormatarCpfOuCnpj(u.CpfOrCnpjUser) AS 'CPF/CNPJ do Usuário',
-		 u.GenUser AS 'Gênero do Usuário'
+		 u.GenUser AS 'Gênero do Usuário',
+		 u.PronomeUser AS 'Pronome do Usuário'
 FROM users u
 WHERE u.StsAtivoUser = 'n';
