175e641234c71ddc48b06abd19cedb00800b4bfe
ericNKS <ek.silva.santos@gmail.com>
2025-06-23 20:32:40 -0300

    fix: corrigindo a nomeclatura para ficar igual ao banco de dados

--- src/modules/Consulta/Entities/Consulta.ts
+++ src/modules/Consulta/Entities/Consulta.ts
@@ -3,6 +3,7 @@ export default class Consulta {
     public IdPaciente: number = 0;
     public IdEspecialista: number = 0;
     public DtConsulta: Date = new Date();
+    public DiaSemanaConsulta: number = 0;
     public HrConsulta: string = '';
     public InfoConsulta: string = '';
     public StsAtivoConsulta: string = 'n';
@@ -13,6 +14,7 @@ export default class Consulta {
             IdPaciente: this.IdPaciente,
             IdEspecialista: this.IdEspecialista,
             DtConsulta: this.DtConsulta.toISOString(),
+            DiaSemanaConsulta: this.DiaSemanaConsulta,
             HrConsulta: this.HrConsulta,
             InfoConsulta: this.InfoConsulta,
             StsAtivoConsulta: this.StsAtivoConsulta,
--- src/modules/Consulta/Entities/Expediente.ts
+++ src/modules/Consulta/Entities/Expediente.ts
@@ -1,15 +1,17 @@
 export default class Expediente {
     public Idexpediente: number = 0;
-    public DtExpediente: Date = new Date();
-    public Hrexpediente: string = '';
-    public Stsativoexpediente: string = 's';
     public Iduser: number = 0
+    public DtExpediente: Date = new Date();
+    public HrInicioExpediente: string = '';
+    public HrFinalExpediente: string = '';
+    public Stsativoexpediente: string = 's';
 
     public toJson(): Record<string, any> {
         return {
             Idexpediente: this.Idexpediente,
             DtExpediente: this.DtExpediente.toISOString(),
-            Hrexpediente: this.Hrexpediente,
+            HrInicioExpediente: this.HrInicioExpediente,
+            HrFinalExpediente: this.HrFinalExpediente,
             Stsativoexpediente: this.Stsativoexpediente,
             Iduser: this.Iduser
         }
