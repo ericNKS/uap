2e354aae1b7b28a5afcc3c1b6b4d6eb610e36c52
ericNKS <ek.silva.santos@gmail.com>
2025-06-29 21:59:40 -0300

    feat: adicionando a adição da descricao

--- src/modules/Auth/Controller/EspecialistasController.ts
+++ src/modules/Auth/Controller/EspecialistasController.ts
@@ -43,4 +43,45 @@ export default class EspecialistaController {
             if (connection) connection.release();
         }
     }
+
+    static async adicionarDescricao(
+        req: FastifyRequest,
+        reply: FastifyReply,
+    ) {
+        let connection;
+        const query = `
+            CALL spListarEspecialistasAtivos()
+        `;
+    
+        try {
+            connection = await Database.getConnection();
+            
+            const resultRaw = await connection.execute<mysql.RowDataPacket[]>(query);
+            const result = resultRaw[0][0];
+
+            const especialistas = result.map((user: {
+                IdUser: number;
+                NomeUser: string;
+                EmailUser?: string;
+                TelUser: string;
+                CpfOrCnpjUser?: string;
+                CrpUser: string;
+                DescricaoUser: string;
+                ImgUrlUser: string | null;
+                GenUser: string;
+                PronomeUser: string;
+            }) => {
+                const newUser = { ...user }; 
+                delete newUser.EmailUser;
+                delete newUser.CpfOrCnpjUser;
+                return newUser;
+            });
+
+            return reply.send(especialistas);
+        } catch (error) {
+            throw error;
+        } finally {
+            if (connection) connection.release();
+        }
+    }
 }
--- src/routes/Api.ts
+++ src/routes/Api.ts
@@ -47,6 +47,8 @@ export const EspecialistaRoute = (app:FastifyInstance) => {
     // Office Hours
     const rule = ['RULE_ADMIN', 'RULE_ESPECIALISTA_ATIVO'];
 
+    app.patch('/api/especialista', handleRule(), EspecialistaController.index.bind(EspecialistaController));
+
     app.post('/api/expedientes', handleRule(rule), OfficeHoursController.store.bind(OfficeHoursController));
     app.patch('/api/expedientes/:id', handleRule(rule), OfficeHoursController.changeStatusOfficeHours.bind(OfficeHoursController));
 
